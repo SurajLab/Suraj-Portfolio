f55a98323dccd9eae5de7b21462c9b9e216b7395
SurajLAb <Surajagrawal588@gmail.com>
2025-12-30 19:52:28 +0530

    url change

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,7 +18,7 @@ const PROJECTS = [
   ss: '/portfolio.png',
   tech: ['React', 'Framer Motion', 'Tailwind CSS'],
   live: '#',
-  code: 'https://github.com/kunj2803/Kunj-Portfolio'
+  code: 'https://github.com/SurajLab/Suraj-Portfolio'
   },
 ]
 
